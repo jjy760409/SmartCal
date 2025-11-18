44cd95474684bb96f1d77c7610ef1cf46f059905
jjy760409 <jys760409@naver.com>
2025-11-18 15:17:38 +0900

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const video = document.getElementById("video");
-const btn = document.getElementById("cameraBtn");
-
-btn.addEventListener("click", async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-  video.srcObject = stream;
-});
